a5f0781a4b2fd0cc65571138e6451b29a207bdd4
Cooper Adams <cooperadamsbusiness@gmail.com>
2021-12-02 13:34:23 -0500

    GUI is almost complete. Need to add an = button and rearrange the button layout to fit.

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Initial functions to be built for ease of use, then to be converted for GUI functionality later on.
 
 The calculator will need to be able to add, subtract, divide, and multiply. The operate() function will take in two numbers and then call a function to get the desired operation.
+
+Step 2 is to create the frontend, a calculator. It will be a stylized box containing an appropriate amount of buttons (likely 15 when finished).
+
+The look for the calculator is finalized, and makes use of the color palette found here: https://www.color-hex.com/color-palette/1006144
+
+Step 3 is to add functionality to the buttons. This will entail on-click event listeners and updating the display to match what has been clicked.
--- functions.js
+++ functions.js
@@ -1,3 +1,126 @@
+const prevChar = '0';
+
+//Gets access to the calculator's display and defaults it to 0
+const screen = document.getElementById('screenDisplay');
+screen.textContent = "0";
+
+//Gets the button box div and then populates it with buttons
+const btnBox = document.querySelector('.buttons');
+for (let i = 1; i <= 16; ++i)
+{
+    //Buttons for numbers 1-9
+    if (i < 10)
+    {
+        const button = document.createElement('button');
+        button.setAttribute('id', 'number ' + i);
+        button.textContent = i;
+        button.addEventListener('click', addToDisplay);
+        btnBox.appendChild(button);
+    }
+
+    //Button for number 0
+    else if (i == 10)
+    {
+        const button = document.createElement('button');
+        button.setAttribute('id', 'number 0');
+        button.textContent = '0';
+        button.addEventListener('click', addToDisplay);
+        btnBox.appendChild(button);
+    }
+
+    //Button for +
+    else if (i == 11)
+    {
+        const button = document.createElement('button');
+        button.setAttribute('id', 'plusButton');
+        button.textContent = '+';
+        button.addEventListener('click', addToDisplay);
+        btnBox.appendChild(button);
+    }
+
+    //Button for -
+    else if (i == 12)
+    {
+        const button = document.createElement('button');
+        button.setAttribute('id', 'minusButton');
+        button.textContent = '-';
+        button.addEventListener('click', addToDisplay);
+        btnBox.appendChild(button);
+    }
+
+    //Button for /
+    else if (i == 13)
+    {
+        const button = document.createElement('button');
+        button.setAttribute('id', 'divideButton');
+        button.textContent = '/';
+        button.addEventListener('click', addToDisplay);
+        btnBox.appendChild(button);
+    }
+
+    //Button for *
+    else if (i == 14)
+    {
+        const button = document.createElement('button');
+        button.setAttribute('id', 'multiplyButton');
+        button.textContent = '*';
+        button.addEventListener('click', addToDisplay);
+        btnBox.appendChild(button);
+    }
+
+    //Button for . (decimal)
+    else if (i == 15)
+    {
+        const button = document.createElement('button');
+        button.setAttribute('id', 'deciButton');
+        button.textContent = '.';
+        button.addEventListener('click', addToDisplay);
+        btnBox.appendChild(button);
+    }
+
+    //Button for C (clear)
+    else if (i == 16)
+    {
+        const button = document.createElement('button');
+        button.setAttribute('id', 'clearButton');
+        button.textContent = 'C';
+        button.addEventListener('click', addToDisplay);
+        btnBox.appendChild(button);
+    }
+}
+
+//Function for on button click, adds the value of the clicked button
+//to the display.
+function addToDisplay(e)
+{
+    //Clears the display and defaults to 0
+    if (e.target.id == 'clearButton')
+    {
+        screen.textContent = "0";
+    }
+
+    //Removes zero and starts with whatever button was pressed if 0 is
+    //only character in the display
+    else if (screen.textContent == '0')
+    {
+        if (e.target.textContent == '+', '-', '*', '/')
+        {
+            screen.innerHTML = screen.textContent + e.target.textContent;
+            return;
+        }
+
+        screen.innerHTML = e.target.textContent;
+    }
+
+    //Adds whatever button was pressed to the display
+    else
+    {
+        screen.innerHTML = screen.textContent + e.target.textContent;
+    }
+}
+
+//Takes in a number, operator, and another number, then decides which
+//operation to perform based on the operater.
 function operate(num1, operater, num2)
 {
     if (operater == '+')
@@ -26,21 +149,25 @@ function operate(num1, operater, num2)
     }
 }
 
+//Simple addition function
 function add(num1, num2)
 {
     return num1 + num2;
 }
 
+//Simple subtraction function
 function subtract(num1, num2)
 {
     return num1 - num2;
 }
 
+//Simple division function
 function divide(num1, num2)
 {
     return num1 / num2;
 }
 
+//Simple multiplication function
 function multiply(num1, num2)
 {
     return num1 * num2;
